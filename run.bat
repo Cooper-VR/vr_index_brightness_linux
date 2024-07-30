8e8f7efff4d5e262ff8880df6317cbe514885892
Python1320 <python1320@users.noreply.github.com>
2024-07-30 15:59:31 +0300

    remove extra file

--- run.bat
+++
@@ -1,8 +0,0 @@
-setlocal
-cd data
-set PATH=%PATH%;bin\Windows\x64
-set LUA_PATH=./?.lua;./?/?.lua
-set LUA_CPATH=./?.dll
-bin\Windows\x64\luajit.exe run.lua > ..\out.txt 2> ..\err.txt
-cd ..
-endlocal
